15dff3e79ee093d55f153327bf70e580571a2e81
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-01-13 16:07:46 +0200

    Seed Database

---
+++ PlatformService/Data/Seeder.cs
@@ -0,0 +1,50 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public static class Seeder
+{
+    public static void SeedDatabase(this IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        
+        SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+    }
+
+    private static void SeedData(AppDbContext? context)
+    {
+        if (context is null) throw new ArgumentNullException(nameof(context));
+
+        if (context.Platforms.Any()) return;
+
+        var platforms = new List<Platform>()
+        {
+            new Platform()
+            {
+                Name = "Platform A",
+                Cost = "153.29",
+                Publisher = "Oracle Inc."
+            },
+            new Platform()
+            {
+                Name = "Platform B",
+                Cost = "143.1",
+                Publisher = "IBM"
+            },
+            new Platform()
+            {
+                Name = "Platform C",
+                Cost = "13.35",
+                Publisher = "Microsoft"
+            },
+            new Platform()
+            {
+                Name = "Platform D",
+                Cost = "15.29",
+                Publisher = "Apple"
+            }
+        };
+        
+        context.Platforms.AddRange(platforms);
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.Data.Contracts;
+using PlatformService.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>(); 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -20,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.SeedDatabase();
 
 app.UseHttpsRedirection();
 
