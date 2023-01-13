f86accb75e859b2a0602c4a8452c3c450ea0b154
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-01-13 15:18:32 +0200

    Delete WeatherForecast files

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,12 +1,4 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
