1aeffffec3d6aa80c5074740088e8100380a9332
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-01-13 16:50:54 +0200

    GetPlatformById

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -34,4 +34,23 @@ public class PlatformsController : ControllerBase
 
         return Ok(_mapper.Map<IEnumerable<ReadPlatformDto>>(platforms));  
     }
+    
+    /// <summary>
+    /// Returns a platform by ID.
+    /// </summary>
+    /// <returns>OK with platform if platform exists. Not found if platform does not exist. </returns>
+    /// <remarks>
+    /// Add remarks here. 
+    /// </remarks>
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<ReadPlatformDto> GetPlatformById(int id)
+    {
+        var platform = _repository.Get(id);
+
+        if (platform is null) return NotFound();
+        
+        return Ok(_mapper.Map<ReadPlatformDto>(platform));  
+    }
 }
