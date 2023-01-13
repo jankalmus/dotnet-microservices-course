0e615baaf6679df68703ccd0eb18851dd47a424b
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-01-13 16:31:48 +0200

    AutoMapper

---
+++ PlatformService/Profile/PlatformProfile.cs
@@ -0,0 +1,14 @@
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profile;
+
+public class PlatformProfile : AutoMapper.Profile
+{
+    public PlatformProfile()
+    {
+        // Source --> Target 
+        CreateMap<Platform, ReadPlatformDto>();
+        CreateMap<CreatePlatformDto, Platform>();
+    }
+}
